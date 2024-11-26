c2f00e5629c4b0cd8edbe37968078f352d3490c2
Vactor0911 <vactor0911@gmail.com>
2024-11-27 01:07:05 +0900

    Mui 표 요소 구현

---
+++ src/components/StyledTable.tsx
@@ -0,0 +1,101 @@
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  tableCellClasses,
+  TextField,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { color } from "../utils/theme";
+import { useAtom, useAtomValue } from "jotai";
+import { emailsAtom, isInputActiveAtom } from "../state";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: color.primary,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+const StyledTable = () => {
+  const [emails, setEmails] = useAtom(emailsAtom);
+  const isInputActive = useAtomValue(isInputActiveAtom);
+
+  return (
+    <Table stickyHeader>
+      <TableHead>
+        <TableRow>
+          <StyledTableCell align="center" sx={{ fontWeight: "bold" }}>
+            번호
+          </StyledTableCell>
+          <StyledTableCell align="center" sx={{ fontWeight: "bold" }}>
+            메일 제목
+          </StyledTableCell>
+          <StyledTableCell align="center" sx={{ fontWeight: "bold" }}>
+            점수
+          </StyledTableCell>
+          <StyledTableCell align="center" sx={{ fontWeight: "bold" }}>
+            결과
+          </StyledTableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {emails.map((row, index) => (
+          <StyledTableRow key={index}>
+            <StyledTableCell align="center">{index + 1}</StyledTableCell>
+            <StyledTableCell component="th" scope="row" align="center">
+              <TextField
+                id="tf-email-title"
+                variant="standard"
+                fullWidth
+                disabled={!isInputActive}
+                defaultValue={row.title}
+                onChange={(e) => {
+                  setEmails(
+                    emails.map((email, i) => {
+                      if (i === index) {
+                        return { ...email, title: e.target.value };
+                      }
+                      return email;
+                    })
+                  );
+                }}
+              />
+            </StyledTableCell>
+            <StyledTableCell
+              align="center"
+              sx={{ color: row.result == "스팸" ? color.red : "" }}
+            >
+              {row.score}
+            </StyledTableCell>
+            <StyledTableCell
+              align="center"
+              sx={{
+                fontWeight: "bold",
+                color: row.result == "스팸" ? color.red : "",
+              }}
+            >
+              {row.result}
+            </StyledTableCell>
+          </StyledTableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default StyledTable;
