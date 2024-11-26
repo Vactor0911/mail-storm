b2e0bb7c8e9f0db944395253175ea09036cc0ccf
Vactor0911 <vactor0911@gmail.com>
2024-11-27 01:06:40 +0900

    테마 색상 설정

---
+++ src/utils/theme.ts
@@ -0,0 +1,37 @@
+import { alpha, createTheme } from "@mui/material";
+import { red, lightGreen, grey } from "@mui/material/colors";
+
+export const color = {
+  primary: "#24292e",
+  gray: grey[400],
+  green: lightGreen[800],
+  red: red[500],
+};
+
+// 테마 색상
+declare module "@mui/material/styles" {
+  interface Palette {
+    modern: Palette["primary"];
+  }
+
+  interface PaletteOptions {
+    modern?: PaletteOptions["primary"];
+  }
+}
+
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    modern: true;
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const theme = createTheme({
+  palette: {
+    modern: {
+      main: color.primary,
+      light: alpha(color.primary, 0.5),
+      dark: alpha(color.primary, 0.9),
+    },
+  },
+});
