3ae6ffc3e932f788effbefa5d0ffdca3ef7342c0
Vactor0911 <vactor0911@gmail.com>
2024-11-28 00:41:37 +0900

    원 그래프 스타일 수정

--- src/components/StyledChart.tsx
+++ src/components/StyledChart.tsx
@@ -1,19 +1,39 @@
 import { PieChart, pieArcLabelClasses } from "@mui/x-charts/PieChart";
 import { color } from "../utils/theme";
 import { useAtomValue } from "jotai";
-import { analysisResultAtom } from "../state";
+import { emailsAtom } from "../state";
+import { useEffect, useState } from "react";
 
 const palette = [color.green, color.red];
 
 const StyledChart = () => {
-  const analysisResult = useAtomValue(analysisResultAtom);
+  const emails = useAtomValue(emailsAtom);
+  const [resultData, setResultData] = useState<
+    { value: number; label: string }[]
+  >([]);
+
+  useEffect(() => {
+    if (emails.filter(email => email.result !== undefined).length === 0) {
+      setResultData([]);
+      return;
+    }
+
+    const countSpam = emails.filter((email) => email.result === "spam").length;
+    const spamRate = Math.round((countSpam / emails.length) * 100);
+    const hamRate = 100 - spamRate;
+
+    setResultData([
+      { value: hamRate, label: "정상" },
+      { value: spamRate, label: "스팸" },
+    ]);
+  }, [emails]);
 
   return (
     <PieChart
       colors={palette}
       series={[
         {
-          data: analysisResult,
+          data: resultData,
           highlightScope: { fade: "global", highlight: "item" },
           faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
           paddingAngle: 3,
